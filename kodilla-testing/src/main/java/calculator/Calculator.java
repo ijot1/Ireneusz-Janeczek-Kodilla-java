a515acbd0d426a56dc9da6bf8013b8afb9633d79
ijot1 <ireneuszjaneczek128@gmail.com>
2019-11-09 23:22:37 +0100

    Simple calculator unit tests added to TestingMain class

---
+++ kodilla-testing/src/main/java/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Calculator {
+
+        public int add(int a, int b) {
+            int sum = a + b;
+        return sum;
+    }
+
+        public int subtract(int a, int b) {
+        int product = a - b;
+        return product;
+    }
+}
