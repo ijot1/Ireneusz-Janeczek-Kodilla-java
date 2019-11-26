e84e8751692522dbfb01c5990d0441f720d01d1d
ijot1 <ireneuszjaneczek128@gmail.com>
2019-11-26 18:46:42 +0100

    ArrayOperations task added

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -1,12 +1,15 @@
 package com.kodilla.stream.array;
 
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
     static double getAverage(int[] numbers) {
-        int elements = IntStream.range(0, numbers.length)
-                .collect(Collectors.toList())
-                .forEach(System.out::println(elements));
+        return IntStream.range(0, numbers.length)
+                .map(n -> numbers[n])
+                .map(n -> {
+                    System.out.println(n);
+                    return n;
+                })
+                .average().orElse(0);
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -1,26 +1,28 @@
 package com.kodilla.stream.array;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.stream.IntStream;
 
 public class ArrayOperationsTestSuite {
     @Test
-    public void testGetAverage(int[] numbers) {
+    public void testGetAverage() {
 
         //Given
         int tabSize = 20;
-        numbers = new int[tabSize];
+        double sum = 0;
+        int[] numbers = new int[tabSize];
         for (int i = 0; i < tabSize; i++) {
             numbers[i] = (i + 1) * 3;
+            sum += numbers[i];
         }
 
         //When
         double average = ArrayOperations.getAverage(numbers);
 
         //Then
-
-        double funcAverage = IntStream.range(0, numbers.length).sum()/numbers.length;
-
+        double funcAverage = sum/numbers.length;
+        Assert.assertEquals(funcAverage, average, 0.001);
     }
 }
