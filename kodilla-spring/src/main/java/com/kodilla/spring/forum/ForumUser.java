9f839db260d48ec5371f82db5dcc61237c2df386
ijot1 <ireneuszjaneczek128@gmail.com>
2020-02-07 22:07:28 +0100

    Bean ForumUser added

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,16 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+    public String username;
+
+    public ForumUser() {
+        this.username = "John Smith";
+    }
+
+    public String getName() {
+        return "John Smith";
+    }
+}
