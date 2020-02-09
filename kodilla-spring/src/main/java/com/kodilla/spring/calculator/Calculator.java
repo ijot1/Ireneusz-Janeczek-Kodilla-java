0a5a812520fb1f5a466bedd6bdd5488a708b5887
ijot1 <ireneuszjaneczek128@gmail.com>
2020-02-09 12:13:33 +0100

    Calculator with Display added

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -0,0 +1,37 @@
+package com.kodilla.spring.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Calculator {
+    @Autowired
+    private Display display;
+
+
+    public double add(double number1, double number2) {
+        double outcome = number1 + number2;
+        System.out.println("Add" + ": " + number1 + " + " + number2 + ";");
+        display.displayValue(outcome);
+        return outcome;
+    }
+    public double sub(double number1, double number2) {
+        double outcome = number1 - number2;
+        System.out.println("Subtract: " + number1 + " - " + number2 + ";");
+        display.displayValue(outcome);
+        return outcome;
+    }
+    public double mul(double number1, double number2) {
+        double outcome = number1 * number2;
+        System.out.println("Multiply: " + number1 + " * " + number2 + ";");
+        display.displayValue(outcome);
+        return outcome;
+    }
+    public double div(double number1, double number2) {
+        double outcome = number1 / number2;
+        System.out.println("Divide: " + number1 + " / " + number2 + ";");
+        display.displayValue(outcome);
+        return outcome;
+    }
+
+}
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,55 @@
+package com.kodilla.spring.calculator;
+
+import com.kodilla.spring.library.Library;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+    @Test
+    public void testCalculatorAdd() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double outcome = calculator.add(6.2, 5.1);
+        //Then
+        Assert.assertEquals(11.3, outcome, 0.01);
+    }
+    @Test
+    public void testCalculatorSub() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double outcome = calculator.sub(5.1, 6.2);
+        //Then
+        Assert.assertEquals(- 1.1, outcome, 0.01);
+    }
+    @Test
+    public void testCalculatorMul() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double outcome = calculator.mul(6.2, 2.0);
+        //Then
+        Assert.assertEquals(12.4, outcome, 0.01);
+    }
+    @Test
+    public void testCalculatorDiv() {
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        double outcome = calculator.div(6.2, 2.0);
+        //Then
+        Assert.assertEquals(3.1, outcome, 0.01);
+    }
+}
