2d48884842ddcf4df3d1a6912f4902e53e4625b7
ijot1 <ireneuszjaneczek128@gmail.com>
2020-02-21 21:53:12 +0100

    Db with issue

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -0,0 +1,12 @@
+package com.kodilla.jdbc;
+
+public class DbManagerTestSuite {
+    @Test
+    public void testConnect() throws SQLException {
+        //Given
+        //When
+        DbManager dbManager = DbManager.getInstance();
+        //Then
+        Assert.assertNotNull(dbManager.getConnection());
+    }
+}
