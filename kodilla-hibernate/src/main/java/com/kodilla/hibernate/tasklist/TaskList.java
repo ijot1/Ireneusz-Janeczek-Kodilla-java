4ba95c9c33a492bb2adaa985ff6aeb2099bb5421
ijot1 <ireneuszjaneczek128@gmail.com>
2020-02-25 23:31:46 +0100

    Task 17.3 added - errorneous

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,15 +1,19 @@
 package com.kodilla.hibernate.tasklist;
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLIST")
 public class TaskList {
-
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
     public TaskList() {
     }
@@ -37,6 +41,16 @@ public class TaskList {
         return description;
     }
 
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
     private void setId(int id) {
         this.id = id;
     }
@@ -47,5 +61,9 @@ public class TaskList {
 
     private void setDescription(String description) {
         this.description = description;
+    }
+
+    private void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
     }
 }
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,43 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    private InvoiceDao invoiceDao;
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Product p1 = new Product("ski shoes");
+        Product p2 = new Product("helmet");
+        Product p3 = new Product("gloves");
+        Item it1 = new Item(p1, new BigDecimal(1500), 1);
+        Item it2 = new Item(p2, new BigDecimal(650), 1);
+        Item it3 = new Item(p3, new BigDecimal(350), 2);
+        Invoice invoice = new Invoice("Inv_02_01_20", new ArrayList<>());
+        invoice.getItems().add(it1);
+        invoice.getItems().add(it2);
+        invoice.getItems().add(it3);
+
+        //When
+        invoiceDao.save(invoice);
+        int id = invoice.getId();
+
+        //THEN
+        Assert.assertNotEquals(0, 1);
+
+        //CleanUp
+//        invoiceDao.deleteById(id);
+    }
