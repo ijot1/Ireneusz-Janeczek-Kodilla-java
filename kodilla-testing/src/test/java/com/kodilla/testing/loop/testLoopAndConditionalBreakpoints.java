67ac439b2f0fd412d3bf9ff9f342be9c41b1f857
ijot1 <ireneuszjaneczek128@gmail.com>
2020-05-21 18:57:30 +0200

    EBayTestingApp class added

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing.loop;
+
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class testLoopAndConditionalBreakpoints {
+    @Test
+    public void testLoop() {
+        //Given
+        long sum = 0;
+        //When
+        for (int n = 0; n < 1000; n++) {
+            sum += n;
+            System.out.println("[" + n + "] Sum equals: " + sum);
+        }
+        //Then
+        assertEquals(499500, sum);
+    }
+}
